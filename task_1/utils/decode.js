a0dace7007965f31ee53a53215f4803f20cf87df
Roman Ivanov <romaivanov1987@gmail.com>
2024-04-22 14:48:09 +0500

    Add: вывод уникальных id, которые были декодированы

--- task_1/utils/decode.js
+++ task_1/utils/decode.js
@@ -4,18 +4,45 @@ function isDecodableKey(key) {
   return key.endsWith("Id") && !EXCLUDED_FIELDS.includes(key);
 }
 
-function decodedValueForKey(key, value, translations) {
-  return isDecodableKey(key) ? translations[parseInt(value)] ?? value : value;
+function decodedValueForKey(key, value, translations, uniqIds) {
+  if (!isDecodableKey(key)) {
+    return value;
+  }
+
+  const decodedValue = translations[parseInt(value)];
+
+  if (decodedValue === undefined) {
+    return value;
+  }
+
+  uniqIds.add(parseInt(value));
+
+  return decodedValue;
 }
 
 export default function decode(encoded, translations) {
-  return encoded.map((item) => {
+  const uniqIds = new Set();
+
+  const decoded = encoded.map((item) => {
     const decodedItem = {};
 
     for (const key in item) {
-      decodedItem[key] = decodedValueForKey(key, item[key], translations);
+      decodedItem[key] = decodedValueForKey(
+        key,
+        item[key],
+        translations,
+        uniqIds
+      );
     }
 
     return decodedItem;
   });
+
+  console.log(
+    `Список уникальных id (которые были декодированы): ${[...uniqIds]
+      .sort((prev, next) => (next > prev ? -1 : 1))
+      .join(", ")}`
+  );
+
+  return decoded;
 }
